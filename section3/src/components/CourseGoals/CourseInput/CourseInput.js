f15db52f4886a534e7845c350b87ecb07d644eee
김한솔 <hsring23@gmail.com>
2023-12-20 22:02:00 +0900

    add section3

--- section3/src/components/CourseGoals/CourseInput/CourseInput.js
+++ section3/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import Button from "../../UI/Button/Button";
 // import styles from "./CourseInput.module";
